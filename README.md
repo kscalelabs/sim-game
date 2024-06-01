01e362f0bd962e02e39491ac74e6ff6f89db72a9
Benjamin Bolte <ben@bolte.cc>
2024-05-31 23:44:29 -0700

    stub for controlling the robot

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# simgame
+# sim-game
 
 ## Getting Started
 
@@ -9,3 +9,7 @@
 git clone git@github.com:kscalelabs/sim-game.git
 cd sim-game && pip install -e '.[dev]'
 ```
+
+### Notes about installing ManiSkill
+
+Ben tried installing ManiSkill to a cluster and found it very difficult. Somehow Allen installed it without any problems though. If you run into issues, you can ask Allen to work his magic.
--- simgame/scripts/control_robot.py
+++ simgame/scripts/control_robot.py
@@ -1,19 +1,24 @@
 """This script lets users interact with the robot in ManiSkill."""
 
 import argparse
+import logging
 
 import gymnasium as gym
 import numpy as np
 from mani_skill.envs.sapien_env import BaseEnv
 from mani_skill.utils.wrappers import RecordEpisode
 
+logger = logging.getLogger(__name__)
 
-def main() -> None:
-    np.set_printoptions(suppress=True, precision=3)
 
+def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-e", "--env-id", type=str, default="PushCube-v1", help="The environment ID of the task you want to simulate"
+        "-e",
+        "--env-id",
+        type=str,
+        default="PushCube-v1",
+        help="The environment ID of the task you want to simulate",
     )
     parser.add_argument("-o", "--obs-mode", type=str, default="none")
     parser.add_argument(
@@ -30,34 +35,44 @@ def main() -> None:
         "--shader",
         default="default",
         type=str,
-        help="Change shader used for rendering. Default is 'default' which is very fast. Can also be 'rt' for ray tracing and generating photo-realistic renders. Can also be 'rt-fast' for a faster but lower quality ray-traced renderer",
+        help=(
+            "Change shader used for rendering. Default is 'default' which is very fast. Can also be 'rt' for ray "
+            "tracing and generating photo-realistic renders. Can also be 'rt-fast' for a faster but lower quality "
+            "ray-traced renderer"
+        ),
     )
     parser.add_argument("--record-dir", type=str)
     parser.add_argument(
-        "-p", "--pause", action="store_true", help="If using human render mode, auto pauses the simulation upon loading"
+        "-p",
+        "--pause",
+        action="store_true",
+        help="If using human render mode, auto pauses the simulation upon loading",
     )
-    parser.add_argument("--quiet", action="store_true", help="Disable verbose output.")
+    parser.add_argument(
+        "--quiet",
+        action="store_true",
+        help="Disable verbose output.",
+    )
     parser.add_argument(
         "-s",
         "--seed",
         type=int,
         help="Seed the random actions and simulator. Default is no seed",
     )
-    args, opts = parser.parse_known_args()
+    args = parser.parse_args()
 
-    # Parse env kwargs
-    if not args.quiet:
-        print("opts:", opts)
+    return args
 
-    eval_str = lambda x: eval(x[1:]) if x.startswith("@") else x
-    env_kwargs = dict((x, eval_str(y)) for x, y in zip(opts[0::2], opts[1::2]))
-    if not args.quiet:
-        print("env_kwargs:", env_kwargs)
-    args.env_kwargs = env_kwargs
 
-    verbose = not args.quiet
+def main() -> None:
+    np.set_printoptions(suppress=True, precision=3)
+
+    args = parse_args()
+    logging.basicConfig(level=logging.INFO if args.quiet else logging.DEBUG)
+
     if args.seed is not None:
         np.random.seed(args.seed)
+
     env: BaseEnv = gym.make(
         args.env_id,
         obs_mode=args.obs_mode,
@@ -66,7 +81,6 @@ def main() -> None:
         render_mode=args.render_mode,
         shader_dir=args.shader,
         sim_backend=args.sim_backend,
-        **args.env_kwargs,
     )
 
     record_dir = args.record_dir
@@ -74,13 +88,14 @@ def main() -> None:
         record_dir = record_dir.format(env_id=args.env_id)
         env = RecordEpisode(env, record_dir, info_on_video=True)
 
-    if verbose:
-        print("Observation space", env.observation_space)
-        print("Action space", env.action_space)
-        print("Control mode", env.unwrapped.control_mode)
-        print("Reward mode", env.unwrapped.reward_mode)
+    logger.debug("Observation space: %s", env.observation_space)
+    logger.debug("Action space: %s", env.action_space)
+    logger.debug("Control mode: %s", env.unwrapped.control_mode)
+    logger.debug("Reward mode: %s", env.unwrapped.reward_mode)
 
     obs, _ = env.reset(seed=args.seed)
+    logger.debug("Initial observation: %s", obs)
+
     env.action_space.seed(args.seed)
     if args.render_mode is not None:
         viewer = env.render()
@@ -89,12 +104,16 @@ def main() -> None:
 
     while True:
         action = env.action_space.sample()
+
+        # TODO: Detect if some key is pressed, and if so, take an action based
+        # on the key press.
+
         obs, reward, terminated, truncated, info = env.step(action)
-        if verbose:
-            print("reward", reward)
-            print("terminated", terminated)
-            print("truncated", truncated)
-            print("info", info)
+
+        logger.debug("Reward: %s", reward)
+        logger.debug("Observation: %s", obs)
+        logger.debug("Terminated: %s", terminated)
+        logger.debug("Info: %s", info)
 
         if args.render_mode is not None:
             env.render()
@@ -102,10 +121,11 @@ def main() -> None:
         if args.render_mode is None or args.render_mode != "human":
             if terminated or truncated:
                 break
+
     env.close()
 
-    if record_dir:
-        print(f"Saving video to {record_dir}")
+    if record_dir is not None:
+        logger.info("Saving video to %s", record_dir)
 
 
 if __name__ == "__main__":
